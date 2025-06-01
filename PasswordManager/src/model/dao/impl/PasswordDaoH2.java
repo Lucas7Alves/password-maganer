19865420da686bef60670d6e84c85283aeea0ee7
Lucas Alves <64313623+Lucas7Alves@users.noreply.github.com>
2025-06-01 16:29:38 -0300

    feat: encrypt passwords into program

--- PasswordManager/src/model/dao/impl/PasswordDaoH2.java
+++ PasswordManager/src/model/dao/impl/PasswordDaoH2.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import model.dao.PasswordDao;
 import model.entity.PasswordEntry;
+import service.PasswordEncrypter;
 import util.DB;
 
 public class PasswordDaoH2 implements PasswordDao {
@@ -21,7 +22,7 @@ public class PasswordDaoH2 implements PasswordDao {
 		try (Connection conn = DB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
 			stmt.setString(1, entry.getServiceName());
 			stmt.setString(2, entry.getUsername());
-			stmt.setString(3, entry.getPasswordEncrypted());
+			stmt.setString(3, PasswordEncrypter.encrypt(entry.getPasswordEncrypted())); 
 			stmt.setInt(4, Integer.parseInt(userId));
 			stmt.executeUpdate();
 		} catch (SQLException e) {
@@ -29,30 +30,32 @@ public class PasswordDaoH2 implements PasswordDao {
 		}
 	}
 
-    @Override
+	@Override
 	public List<PasswordEntry> findAllByUserId(String userId) {
-	    List<PasswordEntry> list = new ArrayList<>();
-	    String sql = "SELECT * FROM password_entry WHERE user_id = ?";
+		List<PasswordEntry> list = new ArrayList<>();
+		String sql = "SELECT * FROM password_entry WHERE user_id = ?";
 
-	    try (Connection conn = DB.getConnection();
-	         PreparedStatement ps = conn.prepareStatement(sql)) {
-	        ps.setString(1, userId);
-	        ResultSet rs = ps.executeQuery();
+		try (Connection conn = DB.getConnection();
+			 PreparedStatement ps = conn.prepareStatement(sql)) {
+			ps.setString(1, userId);
+			ResultSet rs = ps.executeQuery();
 
-	        while (rs.next()) {
-	            PasswordEntry pe = new PasswordEntry(
-	                rs.getString("service_name"),
-	                rs.getString("username"),
-	                rs.getString("password_encrypted")
-	            );
-	            list.add(pe);
-	        }
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    }
+			while (rs.next()) {
+				String decryptedPassword = PasswordEncrypter.decrypt(rs.getString("password_encrypted")); // descriptografar
 
-	    return list;
-	}    
+				PasswordEntry pe = new PasswordEntry(
+					rs.getString("service_name"),
+					rs.getString("username"),
+					decryptedPassword
+				);
+				list.add(pe);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
 
     @Override
     public boolean deleteByServiceName(String userId, String serviceName) {
@@ -64,6 +67,6 @@ public class PasswordDaoH2 implements PasswordDao {
     	} catch (SQLException e) {
     		e.printStackTrace();
     	}
-    	return false;
-    }
+		return false;
+	}
 }
